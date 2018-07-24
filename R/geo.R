22ad0c4076e008fd65ca3f0954b74c4621da02e6
pokyah <thomas.goossens@protonmail.com>
2018-07-24 06:45:28 +0000

    updating errors in mapping fct

--- R/geo.R
+++ R/geo.R
@@ -247,7 +247,7 @@ build.vs.grid.fun <- function(country_code.chr, NAME_1.chr, res.num, geom.chr, s
 #' @param NAME_1.chr a character specifying the NAME_1 value for lower than country level information
 #' @param EPSG.chr A character specifying the EPSG code of the desired Coordiante Reference System (CRS)
 #' @param EPSG.corine.chr A character specifying the EPSG code of the downloaded Corine Data
-#' @path.corine.shapefile.chr A character specifying the path where th corine shapefiles resides
+#' @param path.corine.shapefile.chr A character specifying the path where th corine shapefiles resides
 #' @return a sf data frame containing reclasssified corine land cover data
 build_cover.sf.fun <- function(
   country_code.chr,
@@ -442,66 +442,56 @@ static.ggmap <- function(
   legend.error.bool,
   pretty_breaks.bool,
   title.chr,
+  legend.chr,
   target.chr
 ){
-  
-  library(ggplot2)
-  library(grid)
-  library(maps)
-  library(maptools)
-  library(ggsn)
-  library(RColorBrewer)
-  
-  if(target.chr == "tsa"){ legend_title = "Temperature (°C)"}
-  if(target.chr == "hra"){ legend_title = "Relative humidity (%)"}
-  if(target.chr == "hct"){ legend_title = "Leaves wetness (%)"}
-  
-  if(pretty_breaks.bool == TRUE){
+
+  if (pretty_breaks.bool == TRUE) {
     # inspired by https://timogrossenbacher.ch/2016/12/beautiful-thematic-maps-with-ggplot2-only/
     # prepare legend with pretty breaks
     # compute quantiles from predictions values
-    quantiles <- unique(stats::quantile(gridded.data.df$response,
-                                 probs = seq(0, 1, length.out = 11), na.rm=T))
+    quantiles <- unique(stats::quantile(gridded.data.df[target.chr],
+                                 probs = seq(0, 1, length.out = 11), na.rm = T))
     labels <- c()
     breaks <- unique(round(c(-60,
-                             min(gridded.data.df$response, na.rm = TRUE),
+                             min(gridded.data.df[target.chr], na.rm = TRUE),
                              quantiles,
-                             max(gridded.data.df$response, na.rm = TRUE)), 1))
-    
+                             max(gridded.data.df[target.chr], na.rm = TRUE)), 1))
+
     labels <- paste0(labels, paste0(format(round(breaks, 1), nsmall = 1)))
     labels <- labels[2:length(labels)]
-    gridded.data.df$response_quantiles <- cut(gridded.data.df$response, 
-                                              breaks = breaks, 
-                                              labels = labels, 
+    gridded.data.df$response_quantiles <- cut(gridded.data.df[target.chr],
+                                              breaks = breaks,
+                                              labels = labels,
                                               include.lowest = T)
     breaks_scale <- levels(gridded.data.df$response_quantiles)
     labels_scale <- rev(breaks_scale)
   }
-  if(pretty_breaks.bool == FALSE){
+  if (pretty_breaks.bool == FALSE) {
     # inspired by https://timogrossenbacher.ch/2016/12/beautiful-thematic-maps-with-ggplot2-only/
-    quantiles <- unique(stats::quantile(gridded.data.df$response,
-                                 probs = seq(0, 1, length.out = 11), na.rm=T))
+    quantiles <- unique(stats::quantile(gridded.data.df[target.chr],
+                                 probs = seq(0, 1, length.out = 11), na.rm = T))
     labels <- c()
     labels <- paste0(labels, paste0(format(round(quantiles, 1), nsmall = 1),
                                     " – ",
                                     format(round(quantiles[2:length(quantiles)], 1), nsmall = 1)))
-    labels <- labels[1:length(labels)-1]
-    gridded.data.df$response_quantiles <- cut(gridded.data.df$response,
+    labels <- labels[1:length(labels) - 1]
+    gridded.data.df$response_quantiles <- cut(gridded.data.df[target.chr],
                                                        breaks = quantiles,
                                                        labels = labels,
                                                        include.lowest = T)
   }
-  
+
   ggmap <- ggplot2::ggplot(gridded.data.df) +
     # choose data to display on the layer
     ggplot2::geom_raster(mapping = ggplot2::aes(coords.x1, coords.x2, fill = response_quantiles), na.rm = TRUE, interpolate = T)
   # choose color palette and create a legend with pretty breaks
-  if(pretty_breaks.bool == TRUE){
+  if (pretty_breaks.bool == TRUE) {
     ggmap <- ggmap +
       ggplot2::scale_fill_manual(
         values = rev(RColorBrewer::brewer.pal(n = length(labels_scale), name = "RdYlBu")), # palette to use
         breaks = rev(breaks_scale), # legend breaks
-        name = legend_title,
+        name = legend.chr,
         drop = FALSE,
         labels = labels_scale, # legend labels
         # legend parameters
@@ -522,11 +512,11 @@ static.ggmap <- function(
   # color palette with discrete classes with quantile scale
   if(pretty_breaks.bool == FALSE){
     ggmap <- ggmap +
-      ggplot2::scale_fill_brewer(legend_title, palette = "RdYlBu", direction = -1)
+      ggplot2::scale_fill_brewer(legend.chr, palette = "RdYlBu", direction = -1)
   }
-    
+
   if(layer.error.bool == TRUE){
-    ggmap <- ggmap + 
+    ggmap <- ggmap +
       # display a layer with standard error
       ggplot2::geom_raster(ggplot2::aes(coords.x1, coords.x2, alpha = se), fill = "white", na.rm = TRUE, interpolate = TRUE) +
       # whitening it
@@ -536,7 +526,7 @@ static.ggmap <- function(
         ggmap <- ggmap + ggplot2::guides(fill = ggplot2::guide_legend(order = 1),
                                 alpha = ggplot2::guide_legend(order = 0))
       }
-      
+
   }
   ggmap <- ggmap +
     ggplot2::ggtitle(title.chr) +   # add title
@@ -546,7 +536,7 @@ static.ggmap <- function(
     ggsn::north(boundaries.sf, scale = 0.1, location = "bottomleft",
                 anchor = c(x = 780000, y = 550000), symbol = 12) +
     # add scalebar
-    ggsn::scalebar(boundaries.sf, dist = 50, dd2km = FALSE, model = "GRS80", 
+    ggsn::scalebar(boundaries.sf, dist = 50, dd2km = FALSE, model = "GRS80",
                    st.dist = 0.03, st.size = 4, box.fill = c("black", "white"),
                    box.color = "black", anchor = c(x = 700000, y = 520000)) +
     # add copyright
